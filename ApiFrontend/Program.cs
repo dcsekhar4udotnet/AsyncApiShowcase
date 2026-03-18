7bb9663c9696deec48a67b5ae71da7620dff37df
Dondapati, Chandra <Chandra.Dondapati@azed.gov>
2026-03-17 18:08:45 -0700

    downgrade to .net 9

--- ApiFrontend/Program.cs
+++ ApiFrontend/Program.cs
@@ -21,7 +21,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseStatusCodePagesWithReExecute("/not-found");
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
@@ -31,3 +31,4 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+
